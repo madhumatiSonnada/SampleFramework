3b89c8fbe47707e33e7021225a717555bb82fd89
Hanamantaray Babaleshwar <madhumati217@gmail.com>
2023-10-13 17:45:47 +1100

    Adding new changes

---
+++ src/main/java/com/qa/opencard/pages/cartPage.java
@@ -0,0 +1,10 @@
+package com.qa.opencard.pages;
+
+public class cartPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is cartpage");
+	}
+
+}
