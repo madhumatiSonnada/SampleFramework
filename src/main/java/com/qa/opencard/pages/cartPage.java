eb393bd7653f57710037ff9510cdebc2b04c63c9
Hanamantaray Babaleshwar <madhumati217@gmail.com>
2023-10-13 18:05:05 +1100

    updated cartpage add per comments

--- src/main/java/com/qa/opencard/pages/cartPage.java
+++ src/main/java/com/qa/opencard/pages/cartPage.java
@@ -1,6 +1,15 @@
 package com.qa.opencard.pages;
 
+import org.openqa.selenium.By;
+
 public class cartPage {
+	
+	private By cart=By.id("cart");
+	
+	public void addTocart()
+	{
+		System.out.println("add to cart is done");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
