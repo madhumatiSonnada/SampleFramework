c393252b977637c9943fcd154f9202fab3a9aad1
madhumatiSonnada <147567371+madhumatiSonnada@users.noreply.github.com>
2023-10-23 17:53:16 +1100

    Create fetch.java

---
+++ src/main/java/com/qa/fetch.java
@@ -0,0 +1,11 @@
+package com.qa.opencard.pages;
+
+public class fetch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="This is demo page";
+System.out.println(s);
+	}
+
+}
