d9c82926c3531966b2e163c40b970f8a5e9a71de
Hanamantaray Babaleshwar <madhumati217@gmail.com>
2023-10-26 13:32:05 +1100

    modified

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -31,7 +31,8 @@ public class DriverFactory
 	public static final Logger log=Logger.getLogger(DriverFactory.class);
 	public WebDriver init_driver(Properties prop)
 	{
-		String browsername=prop.getProperty("browser");
+		//String browsername=prop.getProperty("browser");
+		String browsername=System.getProperty("browser");
 		System.out.println("Browser name is " +browsername);
 		log.info("Browser name is " +browsername);
 		highlight=prop.getProperty("highlight").trim();
